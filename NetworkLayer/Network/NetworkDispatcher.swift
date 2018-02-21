44f39ad7cca7070c83c664ff6dffa5e549e8ab8b
mark <mark.chen@ezpay.com.tw>
2018-02-21 17:31:57 +0800

    Update NetworkDispatcher

--- NetworkLayer/Network/NetworkDispatcher.swift
+++ NetworkLayer/Network/NetworkDispatcher.swift
@@ -11,12 +11,7 @@ import PromiseKit
 import Alamofire
 import SwiftyJSON
 
-enum NetworkErrors: String, Error {
-    case noData = "連線失敗"
-    case nilError = "連線失敗，請重新連線"
-    case noInternet = "請確認網路，是否正常"
-    case jsonInitFail = "連線失敗，初始化錯誤"
-}
+
 
 protocol Dispatcher {
     //base url
@@ -34,7 +29,7 @@ class NetworkDispatcher: Dispatcher {
     public func execute(request: Request) -> Promise<Response> {
         return Promise<Response> { resolver in
             Alamofire.request("\(baseUrl)\(request.path)", method: request.method, parameters: request.parameters, encoding: JSONEncoding.default, headers: request.headers).response(completionHandler: { (defaultDataResponse) in
-                resolver.resolve(Response((defaultDataResponse.response,defaultDataResponse.data,defaultDataResponse.error), for: request), defaultDataResponse.error)
+                resolver.fulfill(Response((defaultDataResponse.response,defaultDataResponse.data,defaultDataResponse.error)))
             })
         }
     }
--- NetworkLayer/Network/Response.swift
+++ NetworkLayer/Network/Response.swift
@@ -9,11 +9,18 @@
 import Foundation
 import SwiftyJSON
 
+enum NetworkErrors: String, Error {
+    case noData = "連線失敗"
+    case nilError = "連線失敗，請重新連線"
+    case noInternet = "請確認網路，是否正常"
+    case jsonInitFail = "連線失敗，回傳初始化錯誤"
+}
+
 enum Response {
     case json(_: JSON)
     case error( _: NetworkErrors)
     
-    init(_ response: (urlResponse: HTTPURLResponse?, data: Data?, error: Error?), for request: Request) {
+    init(_ response: (urlResponse: HTTPURLResponse?, data: Data?, error: Error?)) {
         guard response.urlResponse?.statusCode == 200, response.error == nil else {
             self = .error(NetworkErrors.nilError)
             return
